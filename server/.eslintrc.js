3aecc4a90661f8a2fc184c3229a3dbd553525505
Slave <viacheslav.karpunin28@gmail.com>
2024-01-20 06:29:37 +0300

    change config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@trivago/prettier-plugin-sort-imports',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
